57189212eb9e4496401cc8553dabe093251b75d4
Ido Porat <iporat08@cs.huji.ac.il>
2020-04-15 16:36:27 +0300

    15.4.2020 lunch break

--- uthreads.cpp
+++ uthreads.cpp
@@ -24,6 +24,11 @@
 #define SETITIMER_ERR "system error: setitimer error"
 #define INIT_ERR "thread library error: invalid quantum input on init"
 #define BLOCK_ERR "thread library error: main thread cannot be blocked or tid to block not found"
+#define RESUME_ERR "thread library error: tid to resume not found"
+#define BLOCKED_OR_TERMINATED -1
+#define FAILURE -1
+#define SUCCESS 0
+
 
 typedef  std::shared_ptr<Thread> smartThreadPtr;
 
@@ -38,11 +43,35 @@ int totalNumOfQuantum = 0;
 struct itimerval timer;
 sigset_t maskedSet;
 
-void startTimer() {
+
+void switchThreads(int placeHolder){
+//    BLOCK;
+    ++totalNumOfQuantum;
+    if(runningId != BLOCKED_OR_TERMINATED){
+        int ret_val = sigsetjmp(idMap[runningId]->getEnv(),1);
+        printf("SWITCH: ret_val=%d\n", ret_val);//TODO delete
+        if (ret_val == 1) { //TODO 1 == nonzero?
+            //UNBLOCK;
+            return;
+        }
+        readyQ.push_back(runningId);
+    }
+    runningId = readyQ.front();
+    readyQ.pop_front();
+    idMap[runningId]->setState(RUNNING);
+    idMap[runningId]->incrementQuantum();
+    siglongjmp(idMap[runningId]->getEnv(),1);
+}
+
+/**
+ * //TODO
+ * @return
+ */
+void scheduler() {
     struct sigaction sa = {0};
 
     // Install timer_handler as the signal handler for SIGVTALRM.
-    sa.sa_handler = &switchThread;
+    sa.sa_handler = &switchThreads;
     if (sigaction(SIGVTALRM, &sa, NULL) < 0) {
         std::cerr << SIGACTION_ERR << std::endl;
         exit(1);
@@ -66,16 +95,14 @@ void startTimer() {
         std::cerr << SETITIMER_ERR << std::endl;
         exit(1);
     }
-
-    switchThreads();
+    switchThreads(0);
 }
-
 
 int uthread_init(int *quantum_usecs, int size){
     for(int i = 0; i < size; ++i){
         if(quantum_usecs[i] < 0){
             std::cerr << INIT_ERR << std::endl;
-            return -1;
+            return FAILURE;
         }
     }
     sigemptyset(&maskedSet);
@@ -88,8 +115,8 @@ int uthread_init(int *quantum_usecs, int size){
     readyQ.push_back(0);
     sigsetjmp(idMap[0]->getEnv(),1);
     numOfThreads = 1;
-    startTimer();
-    return 0;
+    scheduler();
+    return SUCCESS;
 }
 
 /**
@@ -113,7 +140,7 @@ int uthread_spawn(void (*f)(void), int priority){
     if(id == MAX_THREAD_NUM){
         std::cerr << SPAWN_ERR << std::endl;
         UNBLOCK;
-        return -1;
+        return FAILURE;
     }
     smartThreadPtr newThread(new Thread(id, priority, f));
     ++numOfThreads;
@@ -127,14 +154,16 @@ int uthread_change_priority(int tid, int priority){
     if(idMap.find(tid) == idMap.end() || priority >= numOfPriorities){
         std::cerr << PRIORITY_ERR << std::endl;
         UNBLOCK;
-        return -1;
+        return FAILURE;
     }
     idMap[tid]->setPriority(priority);
     UNBLOCK;
-    return 0; //TODO or return the id of the created thread?
+    return SUCCESS; //TODO or return the id of the created thread?
 }
 
-
+/**
+ * //TODO
+ */
 template<typename T>  // TODO: might npt have to be template!!!
 void removeElement(T& container, int const& tid) {
     auto position = std::find(container.cbegin(), container.cend(), tid);
@@ -143,13 +172,12 @@ void removeElement(T& container, int const& tid) {
     }
 }
 
-
 int uthread_terminate(int tid){
     BLOCK;
     if(idMap.find(tid) == idMap.end()){
         std::cerr << TERMINATE_ERR << std::endl;
         UNBLOCK;
-        return -1;
+        return FAILURE;
     }
 
     if(tid == 0){
@@ -163,53 +191,65 @@ int uthread_terminate(int tid){
 //            readyQ.erase(position);
 //        }
     --numOfThreads;
-    removeElement(readyQ, tid);
+    removeElement(readyQ, tid); //TODO sp points to memory space that was already freed - BUG potential
 
-    if(tid == runningId) { //TODO waiting for an answer from Noa
-        runningId = -1;
+    if(tid == runningId) {
+        runningId = BLOCKED_OR_TERMINATED;
         UNBLOCK;
-        switchThreads();
+        scheduler();
     }
     UNBLOCK;
-    return 0;
+    return SUCCESS;
 }
-
-
 
 int uthread_block(int tid){
     BLOCK;
     if(idMap.find(tid) == idMap.end() || tid == 0){
         std::cerr << BLOCK_ERR << std::endl;
         UNBLOCK;
-        return -1;
+        return FAILURE;
     }
     if(idMap[tid]->getState() == BLOCKED){
         UNBLOCK;
-        return 0;
+        return SUCCESS;
     }
     idMap[tid]->setState(BLOCKED);
     removeElement(readyQ, tid); // remove from READY queue if there
 
     if(tid == runningId){
+        runningId = BLOCKED_OR_TERMINATED;
         UNBLOCK;
+        scheduler();
     }
-
-
-
+    UNBLOCK;
+    return SUCCESS; //TODO if a running thread blocks itself what should be returned?
 }
 
+int uthread_resume(int tid){
+    BLOCK;
+    if(idMap.find(tid) == idMap.end()){
+        std::cerr << RESUME_ERR << std::endl;
+        UNBLOCK;
+        return FAILURE;
+    }
+    if(idMap[tid]->getState() != BLOCKED){
+        UNBLOCK;
+        return SUCCESS;
+    }
+    idMap[tid]->setState(READY);
+    readyQ.push_back(tid);
+    UNBLOCK;
+    return SUCCESS;
+}
 
+int uthread_get_tid(){
+    return runningId;
+}
 
+int uthread_get_total_quantums(){
+    return totalNumOfQuantum;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+int uthread_get_quantums(int tid){
+    return idMap[tid]->getNumOfQuantum();
+}
